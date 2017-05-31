7f69375c5f01dbfeb71240a9aba30d3a7aae4250
risonakamo <hmykanae@gmail.com>
2017-05-31 17:22:36 -0400

    working on db box element

--- resources/app-dev/index.js
+++ resources/app-dev/index.js
@@ -4,18 +4,5 @@ window.onload=main;
 
 function main()
 {
-    var but=document.querySelector(".but");
-    var ibox=document.querySelector(".ibox");
-
-    but.addEventListener("click",(e)=>{
-        ipcRenderer.send("commands",ibox.value);
-    });
-
-    ibox.addEventListener("keydown",(e)=>{
-        if (e.key && e.key=="Enter")
-        {
-            e.preventDefault();
-            ipcRenderer.send("commands",ibox.value);
-        }
-    });
+    
 }
--- resources/app-dev/main.js
+++ resources/app-dev/main.js
@@ -15,22 +15,22 @@ function createWindow()
         slashes:true}));
 }
 
-ipcMain.on("commands",(e,msg)=>{
-    console.log(msg);
-});
+// ipcMain.on("commands",(e,msg)=>{
+//     console.log(msg);
+// });
 
-var db=new datastore({filename:"db.db",autoload:true});
+// var db=new datastore({filename:"db.db",autoload:true});
 
-// db.insert({name:"1",tags:["tag1","tag2"]});
-// db.insert({name:"2",tags:["tag1"]});
+// // db.insert({name:"1",tags:["tag1","tag2"]});
+// // db.insert({name:"2",tags:["tag1"]});
 
-db.find({},(err,docs)=>{
-    console.log(docs);
-});
+// db.find({},(err,docs)=>{
+//     console.log(docs);
+// });
 
-db.find({tags:"tag2"},(err,docs)=>{
-    console.log("----");
-    console.log(docs);
-});
+// db.find({tags:"tag2"},(err,docs)=>{
+//     console.log("----");
+//     console.log(docs);
+// });
 
 app.on("ready",createWindow);
