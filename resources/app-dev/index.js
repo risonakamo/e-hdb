bca279d78bced56c547007bfffec9919f6d13175
risonakamo <hmykanae@gmail.com>
2017-05-31 21:24:01 -0400

    tag parsing/dbbox generator

--- resources/app-dev/index.js
+++ resources/app-dev/index.js
@@ -4,17 +4,30 @@ window.onload=main;
 
 function main()
 {
-    
+    var boxes=[{name:"hello",
+                img:"http://i.imgur.com/eOuukmp.png",
+                tags:["tage1","tag3",]}];
+
+    genBoxes(boxes);
 }
 
-function genTest()
+function genBoxes(data)
 {
-    var boxes=[{name:"hello",img:"http://i.imgur.com/HxIGGIS.jpg",tags:["tage1","tag3",]}];
+    var ipoint=document.querySelector(".db-boxes");
 
-    var boxinsert=document.querySelector(".db-boxes");
-    var a=new dbBox();
-    a.name=boxes[0].name;
-    a.img=boxes[0].img;
+    for (var x=0;x<data.length;x++)
+    {
+        var newbox=new dbBox();
+        newbox.name=data[x].name;
+        newbox.img=data[x].img;
+        newbox.tags=data[x].tags;
+        newbox.type=data[x].type;
 
-    boxinsert.appendChild(a);
+        if (data[x].wide!=undefined)
+        {
+            newbox.wide=1;
+        }
+
+        ipoint.appendChild(newbox);
+    }
 }
