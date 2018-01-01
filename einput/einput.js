45439f31ed21f0aeda5e4724ffbabe6e151fce24
risonakamo <hmykanae@gmail.com>
2018-01-01 15:08:53 -0500

    einput styles panel control logic

--- einput/einput.js
+++ einput/einput.js
@@ -1,6 +1,30 @@
-window.onload=main;
+window.onload=()=>{
+    new einput;
+};
 
-function main()
+class einput
 {
+    constructor()
+    {
+        this.currentPanel=0;
+        this.initSidebar();
+    }
 
+    initSidebar()
+    {
+        var buttons=document.querySelectorAll(".sidebar .button");
+        var panels=document.querySelectorAll(".panels .panel");
+
+        buttons.forEach((x,i)=>{
+            x.addEventListener("click",(e)=>{
+                buttons[this.currentPanel].classList.remove("selected");
+                panels[this.currentPanel].classList.remove("show");
+
+                this.currentPanel=i;
+
+                x.classList.add("selected");
+                panels[i].classList.add("show");
+            });
+        });
+    }
 }
